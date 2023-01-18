200b8e432d150d5da787384ed4c949949249ddd4
Selina Su <phongsu20@gmail.com>
2023-01-17 18:06:47 -0800

    use props for portpolio page

--- src/components/Project.js
+++ src/components/Project.js
@@ -2,38 +2,49 @@ import React from "react";
 
 function Project(props) {
     return (
-        <div class="portfolio-modal modal fade" id="portfolioModal2" tabindex="-1" aria-labelledby="portfolioModal2" aria-hidden="true">
-            <div class="modal-dialog modal-xl">
-                <div class="modal-content">
-                    <div class="modal-header border-0"><button class="btn-close" type="button" data-bs-dismiss="modal" aria-label="Close"></button></div>
-                    <div class="modal-body text-center pb-5">
-                        <div class="container">
-                            <div class="row justify-content-center">
-                                <div class="col-lg-8">
-                                    <h2 class="portfolio-modal-title text-secondary text-uppercase mb-0">{props.name}</h2>
-                                    <div class="divider-custom">
-                                        <div class="divider-custom-line"></div>
-                                        <div class="divider-custom-icon"><i class="fas fa-star"></i></div>
-                                        <div class="divider-custom-line"></div>
+        <>
+            <div className="col-md-6 col-lg-4 mb-5">
+                <div className="portfolio-item mx-auto" data-bs-toggle="modal" data-bs-target={`portfolioModal${props.id}`}>
+                    <div className="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
+                        <div className="portfolio-item-caption-content text-center text-white"><i className="fas fa-plus fa-3x"></i></div>
+                    </div>
+                    <img className="img-fluid" src={`${props.img}`} alt={`${props.title}`} />
+                </div>
+            </div>
+                                        
+            <div class="portfolio-modal modal fade" id={`portfolioModal${props.id}`} tabindex="-1" aria-labelledby={`portfolioModal${props.id}`} aria-hidden="true">
+                <div class="modal-dialog modal-xl">
+                    <div class="modal-content">
+                        <div class="modal-header border-0"><button class="btn-close" type="button" data-bs-dismiss="modal" aria-label="Close"></button></div>
+                        <div class="modal-body text-center pb-5">
+                            <div class="container">
+                                <div class="row justify-content-center">
+                                    <div class="col-lg-8">
+                                        <h2 class="portfolio-modal-title text-secondary text-uppercase mb-0">{props.name}</h2>
+                                        <div class="divider-custom">
+                                            <div class="divider-custom-line"></div>
+                                            <div class="divider-custom-icon"><i class="fas fa-star"></i></div>
+                                            <div class="divider-custom-line"></div>
+                                        </div>
+                                        
+                                        <img class="img-fluid rounded mb-5" src={`${props.img}`} alt={`${props.title}`} />
+                                        <p class="mb-4">
+                                            {props.description}
+                                        </p>
+                                        <a href={`${props.github}`} class="mb-4">Go to Github repository</a> <br />
+                                        <a href={`${props.deployed}`} class="mb-4">Go to deployed application</a> <br /> <br />
+                                        <button class="btn btn-primary" data-bs-dismiss="modal">
+                                            <i class="fas fa-xmark fa-fw"></i>
+                                            Close Window
+                                        </button>
                                     </div>
-                                    
-                                    <img class="img-fluid rounded mb-5" src={`${props.img}`} alt={`${props.title}`} />
-                                    <p class="mb-4">
-                                        {props.description}
-                                    </p>
-                                    <a href={`${props.github}`} class="mb-4">Go to Github repository</a> <br />
-                                    <a href={`${props.deployed}`} class="mb-4">Go to deployed application</a> <br /> <br />
-                                    <button class="btn btn-primary" data-bs-dismiss="modal">
-                                        <i class="fas fa-xmark fa-fw"></i>
-                                        Close Window
-                                    </button>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </>
     );
 }
 
--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,10 +1,10 @@
 import React from "react";
 import techblogPic from "../../assets/img/portfolio/techblog.png";
-import cabinPic from "../../assets/img/portfolio/cabin.png";
-import circusPic from "../../assets/img/portfolio/circus.png";
+import jatePic from "../../assets/img/portfolio/jate.png";
+import nosqlPic from "../../assets/img/portfolio/nosql.png";
 import safePic from "../../assets/img/portfolio/safe.png";
 import submarinePic from "../../assets/img/portfolio/submarine.png";
-import gamePic from "../../assets/img/portfolio/game.png";
+import weatherPic from "../../assets/img/portfolio/weather.png";
 import Project from "../Project";
 
 
@@ -14,8 +14,7 @@ function Portfolio() {
         <section className="page-section portfolio" id="portfolio">
             <div className="container">
                 
-                <h2 className="page-section-heading text-center text-uppercase text-secondary mb-0">Portfolio</h2>
-                
+                <h2 className="page-section-heading text-center text-uppercase text-secondary mb-0">Portfolio</h2>                
                 <div className="divider-custom">
                     <div className="divider-custom-line"></div>
                     <div className="divider-custom-icon"><i className="fas fa-star"></i></div>
@@ -23,77 +22,42 @@ function Portfolio() {
                 </div>
                 
                 <div className="row justify-content-center">
-                    
-                    <div className="col-md-6 col-lg-4 mb-5">
-                        <div className="portfolio-item mx-auto" data-bs-toggle="modal" data-bs-target="#portfolioModal1">
-                            <div className="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
-                                <div className="portfolio-item-caption-content text-center text-white"><i className="fas fa-plus fa-3x"></i></div>
-                            </div>
-                            <img className="img-fluid" src={cabinPic} alt="Cabin" />
-                        </div>
-                    </div>
-                    
-                    <div className="col-md-6 col-lg-4 mb-5">
-                        <div className="portfolio-item mx-auto" data-bs-toggle="modal" data-bs-target="#portfolioModal2">
-                            <div className="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
-                                <div className="portfolio-item-caption-content text-center text-white"><i className="fas fa-plus fa-3x"></i></div>
-                            </div>
-                            <img className="img-fluid" src={techblogPic} alt="TechBlog" />
-                        </div>
-                    </div>
-                    
-                    <div className="col-md-6 col-lg-4 mb-5">
-                        <div className="portfolio-item mx-auto" data-bs-toggle="modal" data-bs-target="#portfolioModal3">
-                            <div className="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
-                                <div className="portfolio-item-caption-content text-center text-white"><i className="fas fa-plus fa-3x"></i></div>
-                            </div>
-                            <img className="img-fluid" src={circusPic} alt="Circus" />
-                        </div>
-                    </div>
-                   
-                    <div className="col-md-6 col-lg-4 mb-5 mb-lg-0">
-                        <div className="portfolio-item mx-auto" data-bs-toggle="modal" data-bs-target="#portfolioModal4">
-                            <div className="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
-                                <div className="portfolio-item-caption-content text-center text-white"><i className="fas fa-plus fa-3x"></i></div>
-                            </div>
-                            <img className="img-fluid" src={gamePic} alt="Game" />
-                        </div>
-                    </div>
-                   
-                    <div className="col-md-6 col-lg-4 mb-5 mb-md-0">
-                        <div className="portfolio-item mx-auto" data-bs-toggle="modal" data-bs-target="#portfolioModal5">
-                            <div className="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
-                                <div className="portfolio-item-caption-content text-center text-white"><i className="fas fa-plus fa-3x"></i></div>
-                            </div>
-                            <img className="img-fluid" src={safePic} alt="Safe" />
-                        </div>
-                    </div>
-                    
-                    <div className="col-md-6 col-lg-4">
-                        <div className="portfolio-item mx-auto" data-bs-toggle="modal" data-bs-target="#portfolioModal6">
-                            <div className="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
-                                <div className="portfolio-item-caption-content text-center text-white"><i className="fas fa-plus fa-3x"></i></div>
-                            </div>
-                            <img className="img-fluid" src={submarinePic} alt="Submarine" />
-                        </div>
-                    </div>
-                </div>
+                    <Project 
+                        id="1"
+                        name="Text Editor PWA application"
+                        description="My text editor is a single page application that runs in the browser and meets PWA criteria. Additionally, it features a number of data persistence techniques that serve as redundancy in case one of the options is not supported by the browser. The application also functions offline. I use idb package, which is a lightweight wrapper around the indexedDB API. It features a number of methods that are useful for storing and retrieving data. App is deployed to Heroku."
+                        github="https://github.com/fuuko08/CH19-PWA-Text-Editor"
+                        deployed="https://jatetexteditorapp.herokuapp.com/"
+                        img={jatePic}
+                    />
+                    <Project 
+                        id="2"
+                        name="Tech Blog MVC Fulltack application"
+                        description="I built this Tech Blog for fellow developers who want to write and read about technical concepts, recent advancement, and new technologies. Using this blog allows users to share their coding knowdledge with each others by posting articles and commenting on other's posts. This app follows MVC paradigm in its architectural structure, using Handlebars.js as the templating language, Sequelize as the ORM, and the express-session npm package for authentication. App is deployed to Heroku."
+                        github="https://github.com/fuuko08/MVC-Tech-Blog"
+                        deployed="https://techblogpro.herokuapp.com/"
+                        img={techblogPic}
+                    />
+                    <Project 
+                        id="3"
+                        name="Social Network NOSQL API"
+                        description="I built an API for a social network application where users can share their thoughts, react to friends' thoughts, and create a friend list. I used Express.js for routing, MongoDB database and Mongoose ODM. This application is back end only and won't be deployed. Please see the walkthought video for functionality demonstration. App is tested using Insomnia."
+                        github="https://github.com/fuuko08/CH18-Social-Network-API"
+                        deployed="https://youtu.be/BUXtTWuJvnM"
+                        img={nosqlPic}
+                    />
+                    <Project 
+                        id="4"
+                        name="Server-Side APIs Weather Dashboard"
+                        description="I built a weather dashboard that run in the browser and features dynamically updated HTML and CSS. I used third party APIs from openweathermap.org to retrieve data. This app also uses localstorage to store any persistent data for later uses."
+                        github="https://github.com/fuuko08/CH06-Selina-Weather-Forecast-App"
+                        deployed="https://fuuko08.github.io/CH06-Selina-Weather-Forecast-App/"
+                        img={weatherPic}
+                    />
+
+                </div>                
             </div>
-        </section>
-        <Project 
-            name="Tech Blog MVC Fulltack application"
-            description="I built this Tech Blog for fellow developers who want to write and read about technical concepts, recent advancement, and new technologies. Using this blog allows users to share their coding knowdledge with each others by posting articles and commenting on other's posts. This app follows MVC paradigm in its architectural structure, using Handlebars.js as the templating language, Sequelize as the ORM, and the express-session npm package for authentication. App is deployed to Heroku."
-            github="https://github.com/fuuko08/MVC-Tech-Blog"
-            deployed="https://techblogpro.herokuapp.com/"
-            img={techblogPic}
-        />
-        <Project 
-            name="Tech Blog MVC Fulltack application"
-            description="I built this Tech Blog for fellow developers who want to write and read about technical concepts, recent advancement, and new technologies. Using this blog allows users to share their coding knowdledge with each others by posting articles and commenting on other's posts. This app follows MVC paradigm in its architectural structure, using Handlebars.js as the templating language, Sequelize as the ORM, and the express-session npm package for authentication. App is deployed to Heroku."
-            github="https://github.com/fuuko08/MVC-Tech-Blog"
-            deployed="https://techblogpro.herokuapp.com/"
-            img={techblogPic}
-        />
+        </section>        
     </>    
     );
 }
