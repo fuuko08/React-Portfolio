17827207e1b09eaba9f38ac8c2a2945a11de4ee4
Selina Su <phongsu20@gmail.com>
2023-01-19 11:42:51 -0800

    service worker"

--- src/index.js
+++ src/index.js
@@ -3,9 +3,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './App.css';
-import reportWebVitals from './reportWebVitals';
-
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-reportWebVitals();
+serviceWorker.unregister();
