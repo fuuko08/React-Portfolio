e754ec866f3279267aeaf599aea21c5dc4ff660d
Selina Su <phongsu20@gmail.com>
2023-01-19 11:35:46 -0800

    still deploy"

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './App.css';
+import reportWebVitals from './reportWebVitals';
 
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+reportWebVitals();
