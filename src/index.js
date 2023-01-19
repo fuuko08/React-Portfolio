9bd0d9e7c4be1d835ce1bc069fcefc79424c3ade
Selina Su <phongsu20@gmail.com>
2023-01-19 13:35:51 -0800

    deploy github

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import './App.css';
+import App from './App';
 import * as serviceWorker from './serviceWorker';
+import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    document.getElementById('root')
+);
 
 serviceWorker.unregister();
+reportWebVitals();
